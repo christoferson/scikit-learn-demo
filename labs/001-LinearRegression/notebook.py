5c589b12e6c6d907d47759df17dc5d043394d7b1
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-07-18 23:03:20 +0900

    notebook

--- labs/001-LinearRegression/notebook.py
+++ labs/001-LinearRegression/notebook.py
@@ -3,6 +3,10 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, accuracy_score
+
 #labs/001-LinearRegression
 icecream_sales_df = pd.read_csv("labs/001-LinearRegression/data/IceCreamData.csv")
 #print(icecream_sales_df)
@@ -29,3 +33,41 @@ plt.show()
 correlation_matrix = icecream_sales_df.corr()
 sns.heatmap(correlation_matrix, annot=True)
 plt.show()
+
+X = icecream_sales_df[['Temperature']]
+y = icecream_sales_df[['Revenue']]
+
+print(f"X: {X.shape} \n{X}")
+print(f"y: {y.shape} \n{y}")
+
+X = np.array(X).astype('float32')
+y = np.array(y).astype('float32')
+
+# Split the Dataset with 20% as test data
+
+X_train , X_test, y_train, y_test = train_test_split(X, y, test_size=2)
+
+# Train the Model
+print("Train the Model")
+
+regression_model_sklearn = LinearRegression(fit_intercept=True) #fit_intercept enable y intercept
+regression_model_sklearn.fit(X_train, y_train)
+
+regression_model_sklearn_accuracy = regression_model_sklearn.score(X_test, y_test)
+print(f"regression_model_sklearn_accuracy={regression_model_sklearn_accuracy}")
+
+print(f"coefficient={regression_model_sklearn.coef_}")
+print(f"intercept={regression_model_sklearn.intercept_}")
+
+
+# Train the Model (Disable y intercept - fit_intercept = False)
+print("Train the Model (Disable y intercept - fit_intercept = False)")
+
+regression_model_sklearn = LinearRegression(fit_intercept=False) #fit_intercept enable y intercept
+regression_model_sklearn.fit(X_train, y_train)
+
+regression_model_sklearn_accuracy = regression_model_sklearn.score(X_test, y_test)
+print(f"regression_model_sklearn_accuracy={regression_model_sklearn_accuracy}")
+
+print(f"coefficient={regression_model_sklearn.coef_}")
+print(f"intercept={regression_model_sklearn.intercept_}")
