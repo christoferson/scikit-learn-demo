0ae0f1f93bbb77fc804ad19a12a535bfa88b0305
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-11-04 18:55:52 +0900

    rename

--- labs/001-LinearRegression/ice_cream_regression.py
+++ labs/001-LinearRegression/ice_cream_regression.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, accuracy_score
+#from sklearn.metrics import mean_squared_error, accuracy_score
 
 #labs/001-LinearRegression
 icecream_sales_df = pd.read_csv("labs/001-LinearRegression/data/IceCreamData.csv")
